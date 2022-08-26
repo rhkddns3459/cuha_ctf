2bcb91d7b39216e33ea5e463cf551a505ea46289
CyberSecurityHat <godofwhiteworld@gmail.com>
2022-08-26 19:50:59 +0900

    08.26_CyberSecurityHat_cuhaCTF

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '1234',
+    password : 'root',
     database : 'cuha_ctf',
     dateStrings: "date",
 });
