dc9b3f52bd971f96e6c05923d38eacce6440a98c
kilkat <rhkddns3459@naver.com>
2022-08-29 11:39:24 +0900

    08.29_kilkat_cuhaCTF

--- controllers/register.ctrl.js
+++ controllers/register.ctrl.js
@@ -2,32 +2,9 @@ const { Router } = require("express");
 const mysql = require("mysql2");
 const bcrypt = require('bcrypt');
 const User = require("../models/user");
-const regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-
-    /*const create_user = (req, res) => {
-        const { email, password, re_password , nickname } = req.body;
-        const point = 0;
-
-        console.log(email);
-        console.log(password);
-        console.log(re_password);
-        console.log(nickname);
-
-        bcrypt.hash(password, 10, (err, password) => {
-            const sql = 'INSERT INTO user(email, password, nickname, point) VALUES(?,?,?,?)';
-            const params = [email, password, nickname, point];
-    
-            connection.query(sql, params, function(err, rows, fields){
-                if(err){
-                    console.log(err);
-                } else{
-                    res.redirect("/");
-                }
-            });    
-        })
-    
-    
-    }*/
+const e = require("express");
+const email_exp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; //email regExp
+const password_exp = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"; //password regExp
 
     const create_user = async(req, res) => {
         const {email, password, re_password, nickname, team} = req.body;
@@ -37,27 +14,34 @@ const regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z]
         console.log(re_password);
         console.log(nickname);
 
+        if(email.match(email_exp) === null){
+            res.send("<script>alert('지정된 이메일 형식을 사용하세요.');location.href='/';</script>");
+        };
+
+        if(password != re_password){
+            res.send("<script>alert('비밀번호가 일치하지 않습니다.');location.href='/register';</script>");
+        };
+
+        if(password.match(password_exp) === null || re_password.match(password_exp === null)){
+            res.send("<script>alert('비밀번호 형식은 알파벳, 숫자, 특수문자 포함입니다.');location.href='/register';</script>");
+        };
+
         try{
             const exUser = await User.findOne({ where: {email}});
 
             console.log(exUser);
 
             if(exUser === null){
-                if(password == re_password) {
                 bcrypt.hash(password, 10, (err, password) => {
                 User.create({
                     email: email,
                     nickname: nickname,
                     password: password,
                     team: team,
-                  });
+                });
                 })
-                res.send("<script>alert('회원가입되었습니다.');location.href='/';</script>");
+                    res.send("<script>alert('회원가입 되었습니다.');location.href='/';</script>");
             }else{
-                
-            res.send("<script>alert('비밀번호가 맞지 않습니다.');location.href='/register';</script>");
-            }
-        }else{
                 res.send("<script>alert('중복된 정보가 있습니다.');location.href='/register';</script>");
             }
             }catch(err){
