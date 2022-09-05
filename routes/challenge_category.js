c26c3709e969542f99e4b72cfc9dffcd26bf5144
CyberSecurityHat <godofwhiteworld@gmail.com>
2022-09-05 16:16:09 +0900

    09.05_CyberSecurityHat_cuhaCTF

--- routes/challenge_category.js
+++ routes/challenge_category.js
@@ -21,19 +21,19 @@ router.get("/reversing", (req, res) => {
     }
 });
 
-router.get("/system", (req, res) => {
-    if(!req.session.is_logined) {
-        res.send("<script>alert('로그인 후 이용해주세요');location.href='/login';</script>");
-    } else {
-    res.render("challenges/challenge_category/category3_system.ejs", {session: req.session});
-    }
-});
+// router.get("/system", (req, res) => {
+//     if(!req.session.is_logined) {
+//         res.send("<script>alert('로그인 후 이용해주세요');location.href='/login';</script>");
+//     } else {
+//     res.render("challenges/challenge_category/category3_system.ejs", {session: req.session});
+//     }
+// });
 
 router.get("/network", (req, res) => {
     if(!req.session.is_logined) {
         res.send("<script>alert('로그인 후 이용해주세요');location.href='/login';</script>");
     } else {
-    res.render("challenges/challenge_category/category4_network.ejs", {session: req.session});
+    res.render("challenges/challenge_category/category3_network.ejs", {session: req.session});
     }
 });
 
@@ -41,23 +41,23 @@ router.get("/crypto", (req, res) => {
     if(!req.session.is_logined) {
         res.send("<script>alert('로그인 후 이용해주세요');location.href='/login';</script>");
     } else {
-    res.render("challenges/challenge_category/category5_crypto.ejs", {session: req.session});
+    res.render("challenges/challenge_category/category4_crypto.ejs", {session: req.session});
     }
 });
 
-router.get("/forensic", (req, res) => {
-    if(!req.session.is_logined) {
-        res.send("<script>alert('로그인 후 이용해주세요');location.href='/login';</script>");
-    } else {
-    res.render("challenges/challenge_category/category6_forensic.ejs", {session: req.session});
-    }
-});
+// router.get("/forensic", (req, res) => {
+//     if(!req.session.is_logined) {
+//         res.send("<script>alert('로그인 후 이용해주세요');location.href='/login';</script>");
+//     } else {
+//     res.render("challenges/challenge_category/category6_forensic.ejs", {session: req.session});
+//     }
+// });
 
 router.get("/misc", (req, res) => {
     if(!req.session.is_logined) {
         res.send("<script>alert('로그인 후 이용해주세요');location.href='/login';</script>");
     } else {
-    res.render("challenges/challenge_category/category7_misc.ejs", {session: req.session});
+    res.render("challenges/challenge_category/category5_misc.ejs", {session: req.session});
     }
 });
 
