ff19714d1669fa8ba4addbc61a7cc97d51470340
PT3000 <t380125a@naver.com>
2022-09-23 23:38:37 +0900

    09.23_yjm_cuhaCTF

--- controllers/challenge_flag.ctrl.js
+++ controllers/challenge_flag.ctrl.js
@@ -4,16 +4,16 @@ const User = require("../models/users");
 const challenge_flag = async(req, res) => {
     const user_flag = req.body.flag;
     const user_type = req.body.type;
-    const nick = req.session.nickname;
-    const user_correct = await User.findOne({where: {nickname:nick}});
-    const flag_correct = await Flag.findOne({attributes: ['flag'],where: {type: user_type},raw:true});
+    const user_correct = await User.findOne({where: {email: req.session.email}});
+    const flag_correct = await Flag.findOne({where: {type: user_type},raw:true});
     if(flag_correct.flag === user_flag) {
         point_num = user_correct.point + flag_correct.point
         User.update({
             point: point_num
           }, {
-            where: { nickname: nick }
+            where: { email: req.session.email }
           });
+        console.log(flag_correct.point);
         res.send("<script>alert('정답입니다.');location.href='/';</script>");
     } else {
         //console.log(flag_correct[0]);
