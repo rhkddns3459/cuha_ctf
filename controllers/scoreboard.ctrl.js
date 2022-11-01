bcdcc6bed21b4f8e2dfd0d704224d8b5ab823e1f
PT3000 <t380125a@naver.com>
2022-11-01 18:25:31 +0900

    11.01_yjm_cuhaCTF

--- controllers/scoreboard.ctrl.js
+++ controllers/scoreboard.ctrl.js
@@ -12,6 +12,7 @@ const user_rank = async(req, res) => {
             res.render("challenges/scoreboard.ejs", {session: req.session, rank: user_info})
     }catch(err){
         console.log("scoreboard 에러가 발생했습니다." + err);
+        res.send("<script>alert('현재 scoreboard에 등록된 사람이 없습니다');location.href='/';</script>");
     }
 }
 
