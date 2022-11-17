183400f0ea2c6d44fd2a2c03862c5f91985560a9
PT3000 <t380125a@naver.com>
2022-11-17 16:15:29 +0900

    11.17_yjm_cuhaCTF

--- routes/settings.js
+++ routes/settings.js
@@ -6,8 +6,12 @@ const User = require("../models/users");
 
 router.get("/", async(req, res) => {
     try{
-    const user_settings = await User.findOne({where: {email: req.session.email}});
-    res.render("members/settings.ejs", {session: req.session, settings: user_settings});
+        if(!req.session.is_logined) {
+            res.send("<script>alert('로그인 후 이용해주세요');location.href='/login';</script>");
+        } else {
+            const user_settings = await User.findOne({where: {email: req.session.email}});
+            res.render("members/settings.ejs", {session: req.session, settings: user_settings});
+        }
     console.log(user_settings)
 }catch(err){
     console.log(err);
